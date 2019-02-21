59dbb3d647fc726958611c11022034d8a71f0736
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-02-21 02:51:30 -0500

    Change deploy script variable

--- deploy-scripts/after_deploy.sh
+++ deploy-scripts/after_deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-aws s3api put-bucket-website --bucket $S3_BUCKET --website-configuration file://s3-config/websiteConfiguration.json --region ca-central-1
+aws s3api put-bucket-website --bucket $S3_BUCKET_NAME --website-configuration file://s3-config/websiteConfiguration.json --region ca-central-1
 
 travis-ci-cloudfront-invalidation -a $AWS_ACCESS_KEY_ID -s $AWS_SECRET_ACCESS_KEY -c $CLOUDFRONT_ID -i '/*' -b $TRAVIS_BRANCH -p $TRAVIS_PULL_REQUEST
 
