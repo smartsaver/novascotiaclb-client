e5a7bfae364f31a71c54ddb8396c74c925798469
Asemco <crasykid2@hotmail.com>
2020-02-24 18:50:08 -0500

    - Reacting the CSS to a fix

--- src/pages-components/index/ApplySection.js
+++ src/pages-components/index/ApplySection.js
@@ -4,13 +4,21 @@ import style from './ApplySection.module.css'
 
 const ApplySection = props => {
   const { embedUrl, title } = props.content
+  const styles = {
+    frame: {
+      width: '100%',
+      margin: '1rem 0',
+      background: 'white',
+      minHeight: '43rem',
+    },
+  };
   return (
     <Section name="apply" className={style.ApplySection}>
       <h2 className="title is-3">{title}</h2>
       <p>If you cannot see the form, <a href="https://mysmartfuture.org/signup/clbnb" target="_blank" rel="noopener noreferrer">click here.</a></p>
       <iframe
         id="resp"
-        style="width:100%; margin:1rem 0; background: white; min-height: 43rem"
+        style={styles.frame}
         src={embedUrl}
         frameBorder="0"
         title="StartMyResp Application"
