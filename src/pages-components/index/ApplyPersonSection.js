1a5c92cc71379ae9e94b000bd07483e77ae4a09b
Asemco <crasykid2@hotmail.com>
2020-03-17 19:51:41 -0400

    - Added COVID-19 Updates

--- src/pages-components/index/ApplyPersonSection.js
+++ src/pages-components/index/ApplyPersonSection.js
@@ -2,16 +2,29 @@ import React from 'react'
 import Section from '../../components/Section'
 import ApplyTable from './ApplyTable'
 
+const styles = {
+  covid: {
+    textAlign: 'center',
+  },
+  fix: {
+    marginTop: 0,
+    marginBottom: 0,
+    paddingTop: 4,
+    paddingBottom: 4
+  }
+};
+
 const ApplyPersonSection = props => {
   const { title, header, table } = props.content
   return (
     <Section>
       <h2 className="title">{title}</h2>
       <div className="content" dangerouslySetInnerHTML={{ __html: header }} />
-      <ApplyTable
-        headers={['City', 'Date and Time', 'Location']}
-        content={table}
-      />
+      <div style={styles.covid}>
+        <h4>
+          Special Update: Due to COVID-19, all Super Clinics have been postponed.
+        </h4>
+      </div>
     </Section>
   )
 }
--- src/pages-components/index/IndexBanner.js
+++ src/pages-components/index/IndexBanner.js
@@ -4,6 +4,7 @@ import style from './IndexBanner.module.css'
 
 const IndexBanner = props => {
   const { content = '', title = '' } = props.content
+  
   return (
     <header>
       <Section
@@ -19,6 +20,12 @@ const IndexBanner = props => {
           dangerouslySetInnerHTML={{ __html: content }}
         />
       </Section>
+      <div className={style.covid}>
+        <h2 className={style.fix}>
+          Due to the COVID-19 disruptions, all families will experience longer than usual wait times to set up an appointment with their bank to complete the application process.
+          The current wait time at some banks is estimated to be 14-30 business days.
+        </h2>
+      </div>
     </header>
   )
 }
